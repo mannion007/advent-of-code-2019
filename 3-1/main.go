a98d3e5fd0983b3884c748110d59ae6cd200110a
James Mannion <james.mannion@pay4later.com>
2019-12-04 00:13:43 +0000

    Problem 3 part 1

--- 3-1/main.go
+++ 3-1/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"math"
 	"strconv"
 	"strings"
-	"math"
-	"io/ioutil"
-	"fmt"
 )
 
 var (
-	xMap = map[string]int{"U": 0,"D": 0,"L": -1,"R": 1,}
-	yMap = map[string]int{"U": 1,"D": -1,"L": 0,"R": 0,}
+	xMap         = map[string]int{"U": 0, "D": 0, "L": -1, "R": 1}
+	yMap         = map[string]int{"U": 1, "D": -1, "L": 0, "R": 0}
 	minManhattan int
 )
 
@@ -27,17 +27,17 @@ func main() {
 	intersect := make(map[vector]struct{})
 	for k, v := range trace1 {
 		if _, ok := trace2[k]; ok {
-    		intersect[k] = v
-    	}
+			intersect[k] = v
+		}
 	}
 
 	for k, _ := range intersect {
 		k.x = int(math.Abs(float64(k.x)))
 		k.y = int(math.Abs(float64(k.y)))
 
-    	if k.x + k.y < minManhattan || minManhattan == 0{
-    		minManhattan = k.x + k.y
-    	}
+		if k.x+k.y < minManhattan || minManhattan == 0 {
+			minManhattan = k.x + k.y
+		}
 	}
 
 	fmt.Println(minManhattan)
@@ -48,15 +48,15 @@ func trace(dirs []string) map[vector]struct{} {
 
 	trace := make(map[vector]struct{})
 
-	for _, d := range(dirs) {
-		
+	for _, d := range dirs {
+
 		dist, _ := strconv.ParseInt(d[1:], 10, 64)
 
 		for i := 0; i < int(dist); i++ {
 			x += xMap[string(d[:1])]
 			y += yMap[string(d[:1])]
 
-			trace[vector{x,y}] = struct{}{}
+			trace[vector{x, y}] = struct{}{}
 		}
 	}
 
@@ -67,8 +67,8 @@ func readFile(fileName string) []string {
 	data, err := ioutil.ReadFile(fileName)
 
 	if err != nil {
-        panic(err)
-    }
+		panic(err)
+	}
 
-    return strings.SplitN(string(data), ",", -1)
+	return strings.SplitN(string(data), ",", -1)
 }
--- 3-1/main_test.go
+++ 3-1/main_test.go
@@ -6,14 +6,13 @@ import (
 
 func TestTraceWire(t *testing.T) {
 
-	input := []string{"R8","U5","L5","D3"}
-	expected := []vector{{0,0},{8,0},{8,5},{3,5},{3,2},}
+	input := []string{"R8", "U5", "L5", "D3"}
+	expected := []vector{{0, 0}, {8, 0}, {8, 5}, {3, 5}, {3, 2}}
 
 	got := traceWire(input)
 
 	if len(got) != len(expected) {
 		t.Errorf("first(%v) = %v; want %v", input, got, expected)
 	}
-
 
 }
